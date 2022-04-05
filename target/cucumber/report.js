511c68b75a5406facac6634440dbc21a53eb55d9
Flipu72 <flipu72@yahoo.com>
2022-04-05 08:24:59 -0500

    1st commit

---
+++ target/cucumber/report.js
@@ -0,0 +1,163 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/features/BackGround.feature");
+formatter.feature({
+  "line": 2,
+  "name": "TechFios Portal Background functionality validation",
+  "description": "",
+  "id": "techfios-portal-background-functionality-validation",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 12524965900,
+  "status": "passed"
+});
+formatter.background({
+  "line": 4,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is on techfios test page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepDefinitions.user_is_on_techfios_test_page()"
+});
+formatter.result({
+  "duration": 1387994500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "User should be able to change background color to sky blue then back to white",
+  "description": "",
+  "id": "techfios-portal-background-functionality-validation;user-should-be-able-to-change-background-color-to-sky-blue-then-back-to-white",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 7,
+      "name": "@Scenario1"
+    }
+  ]
+});
+formatter.step({
+  "line": 9,
+  "name": "button Set SkyBlue Background exists",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "user clicks on blue button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "the background color will change to sky blue",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinitions.button_Set_SkyBlue_Background_exists()"
+});
+formatter.result({
+  "duration": 733584200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinitions.user_clicks_on_blue_button()"
+});
+formatter.result({
+  "duration": 227166100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinitions.the_background_color_will_change_to_sky_blue()"
+});
+formatter.result({
+  "duration": 237813300,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 4544433900,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 5967138500,
+  "status": "passed"
+});
+formatter.background({
+  "line": 4,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is on techfios test page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "StepDefinitions.user_is_on_techfios_test_page()"
+});
+formatter.result({
+  "duration": 751765100,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "User should be able to change background color back to white",
+  "description": "",
+  "id": "techfios-portal-background-functionality-validation;user-should-be-able-to-change-background-color-back-to-white",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 13,
+      "name": "@Scenario2"
+    }
+  ]
+});
+formatter.step({
+  "line": 15,
+  "name": "button Set SkyWhite Background exists",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 16,
+  "name": "user clicks on white button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 17,
+  "name": "the background color will change to white",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinitions.button_Set_SkyWhite_Background_exists()"
+});
+formatter.result({
+  "duration": 487287800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinitions.user_clicks_on_white_button()"
+});
+formatter.result({
+  "duration": 314190700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinitions.the_background_color_will_change_to_white()"
+});
+formatter.result({
+  "duration": 246048100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 10762104100,
+  "status": "passed"
+});
+});
